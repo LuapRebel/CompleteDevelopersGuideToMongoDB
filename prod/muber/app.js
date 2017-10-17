a427dd67f42364a1b67630d569dd78c0675668d0
howard <paulleber23@gmail.com>
2017-10-16 19:16:30 -0700

    Add Section 14 - Part 2

--- prod/muber/app.js
+++ prod/muber/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 const app = express();
 
-// Watch for incoming requests of method GET
-// to the route http://localhost:3050/api
-app.get('/api', (req, res) => {
-	res.send({ hi: 'there' });
-});
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'test') {
+	mongoose.connect('mongodb://localhost/muber', { useMongoClient: true });
+}
 
+app.use(bodyParser.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+	res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
---
+++ prod/muber/test/controllers/drivers_controller_test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../app');
+const Driver = mongoose.model('driver');
+
+describe('Drivers controller', () => {
+	it('POST to /api/drivers creates a new driver', (done) => {
+		Driver.count().then(count => {
+			request(app)
+				.post('/api/drivers')
+				.send({ email: 'test@test.com' })
+				.end(() => {
+					Driver.count().then(newCount => {
+						assert(count + 1 === newCount);
+						done();
+					});
+			});
+		});
+	});
+
+	it('PUT to /api/drivers/id edits an exising driver', (done) => {
+		const driver = new Driver({ email: 't@t.com', driving: false });
+
+		driver.save().then(() => {
+			request(app)
+				.put(`/api/drivers/${driver._id}`)
+				.send({ driving: true })
+				.end(() => {
+					Driver.findOne({ email: 't@t.com' })
+						.then(driver => {
+							assert(driver.driving === true);
+							done();
+						});
+				});
+		});
+	});
+
+	it('DELETE to /api/drivers/id deletes an existing driver', (done) => {
+		const driver = new Driver({ email: 'test@test.com', driving: false });
+
+		driver.save().then(() => {
+			request(app)
+				.delete(`/api/drivers/${driver._id}`)
+				.end(() => {
+					Driver.findOne({ email: 'test@test.com' })
+						.then((driver) => {
+							assert(driver === null);
+							done();
+						});
+				});
+		});
+	});
+});
